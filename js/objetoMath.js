d8d51a3114ee6024dc10d6551629a31e6e350540
Jean <jeanrollero@gmail.com>
2015-11-07 13:46:05 -0200

    Arquivos de estudo do Objeto Math

---
+++ js/objetoMath.js
@@ -0,0 +1,48 @@
+//O OBJETO MATH NÃO TEM UM Médoto CONSTRUTOR
+
+//Valor absoluto de um valor
+document.write("Valor absoluto de 3.8: " + Math.abs(3.8) + "<br />");
+
+//Valor máximo de um conjunto
+document.write("Maior valor do intervalo de (3, 5, 6, 2, 1, 8, 7.7): " + Math.max(3, 5, 6, 2, 1, 8, 7.7) + "<br />");
+
+//Valor minimo de um conjunto
+document.write("Menor valor do intervalo de (3, 5, 6, 2, 1, 8, 7.7) " + Math.min(3, 5, 6, 2, 1, 8, 7.7) + "<br />");
+
+//Mostra o numero inteiro mais pŕoximo do valor
+document.write("Número inteiro mais pŕoximo de 6.3: " + Math.round(6.3) + "<br />");
+
+//Arredonda para baixo para o valor mais próximo de um numero
+document.write("3.8 arredonda para baixo: " + Math.floor(3.8) + "<br />");
+
+//Arredonda para cima para o valor mais próximo de um numero
+document.write("3.8 arredondado para cima: " + Math.ceil(3.8) + "<br />");
+
+//Potencia de um valor (numero, potencia)
+document.write("Potência de 2 elevado a 8: " + Math.pow(2,8) + "<br />");
+
+//Raiz quadrada de um numero
+document.write("Raiz quadrada de 4: " + Math.sqrt(4) + "<br />");
+
+//Retorna um numero randomico
+document.write("Número randomico entre 0 e 1: " + Math.random() + "<br />");
+
+//Gerar numeros aleatórios entre 0 e 10
+var n = Math.floor(Math.random()*10);
+document.write("Aleatório entre 0 e 10: " + n + "<br />");
+
+//Eleva o numero de Euller a potencia desejada
+document.write("Euller a potencia 4: " + Math.exp(4) + "<br />");
+
+//Logaritmo de um valor
+document.write("Logaritmo de 3: " + Math.log(3) + "<br />");
+
+//Seno
+document.write("Seno de angulo 3: " + Math.sin(3) + "<br />");
+
+//Cosseno
+document.write("Cosseno de angulo 3: " + Math.cos(3) + "<br />");
+
+//Tangente
+document.write("Tangente de angulo 3: " + Math.tan(3) + "<br />");
+
