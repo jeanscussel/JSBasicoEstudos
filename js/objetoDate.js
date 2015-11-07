a91606a58eee7cb0f3f15f0f03b3e5d753c26dcc
Jean <jeanrollero@gmail.com>
2015-11-07 12:51:21 -0200

    Aula Objeto Math

--- js/objetoDate.js
+++ js/objetoDate.js
@@ -55,20 +55,42 @@ var data = new Date("Dec/18/2015 11:30:30");
 document.write("Indice do mês: " + data.getMonth() + " - ");
 document.write("Mês do ano: " + meses[data.getMonth()] + "<br />");
 
+//retorna a quantidade tottal de milissegundos do objeto date apartir de 01/01/1970
+var data = new Date("Jul/18/2015 10:30:30");
+document.write("Total de milissegundos: " + data.getTime() + "<br />");
 
+//retorna a diferença de minutos entre hora local e GMT
+var data = new Date("Jul/18/2015 10:30:30");
+document.write("Diferença Hora Brasilia GMT -3 x GMT em Minutos: " + data.getTimezoneOffset() + "<br />");
 
+//Chama direto da classe, retorna quantidade de milissegundos de uma data
+var data = new Date("Jul/18/2015 10:30:30");
+document.write("Quantidade de milissegundos: " + Date.parse(data) + "<br />");
 
+//METODOS PARA ATRIBUIR VALORES = SET
+var data = new Date("Jul/18/2015 10:30:30");
+document.write("Valor da alteração de ano: " + data.setFullYear(2016) + "<br />");
+document.write("Ano alterado: " + data + "<br />");
 
+//SET PARA DIA DO MÊS
+var data = new Date("Jul/18/2015 10:30:30");
+document.write("Dia do mês alterado: " + data.setDate(26) + "<br />");
 
+//SET PARA O MÊS
+var data = new Date("Jul/18/2015 10:30:30");
+document.write("Mês alterado: " + data.setMonth() + "<br />");
 
+//Mudar a data para string
+var data = new Date("Jul/18/2015 10:30:30");
+document.write("Data string: " + data.toDateString() + "<br />");
 
+//Mudar a data para string com conversão do país local
+var data = new Date();
 
-
-
-
-
-
-
+data.setFullYear(2015);
+data.setMonth(07);
+data.setDate(18);
+document.write("Data string formato BRASILEIRO: " + data.toLocaleDateString() + "<br />");
 
 
 
